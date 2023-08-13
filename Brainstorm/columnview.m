e024e27eae81f5baad52b5d10c5332d1f8ca005d
dzakwanilhakim <79700185+dzakwanilhakim@users.noreply.github.com>
2023-08-13 10:35:24 +0700

    Delete Brainstorm directory

--- Brainstorm/columnview.m
+++
@@ -1,7 +0,0 @@
-data=cre_ttest;
-delta=data.pmap(:,:,1);
-Theta=data.pmap(:,:,2);
-Alpha=data.pmap(:,:,3);
-Beta=data.pmap(:,:,4);
-Gamma=data.pmap(:,:,5);
-Recap=[delta, Theta, Alpha, Beta, Gamma];
